83c83205b5a721903c37d600ee5056c5e2a5e5ef
Lain-Iwakura1998 <ilain3902@gmail.com>
2025-09-04 15:49:00 +0800

    Update AccessPoint.java

--- app/src/main/java/com/talentica/wifiindoorpositioning/wifiindoorpositioning/model/AccessPoint.java
+++ app/src/main/java/com/talentica/wifiindoorpositioning/wifiindoorpositioning/model/AccessPoint.java
@@ -7,34 +7,27 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.UUID;
 
-import io.realm.RealmModel;
-import io.realm.RealmObject;
-import io.realm.annotations.PrimaryKey;
-
 /**
  * Created by suyashg on 25/08/17.
  */
+public class AccessPoint implements Parcelable {
 
-public class AccessPoint extends RealmObject implements Parcelable {
-
-    @PrimaryKey
     private String id = UUID.randomUUID().toString();
     private Date createdAt = new Date();
-    private String bssid;//identifier
+    private String bssid;          // identifier
     private String description;
     private String ssid;
     private String mac_address;
     private double x;
     private double y;
     private int sequence;
-    private double meanRss;//for RP (-50 to -100)
-//    High quality: 90% ~= -55db
-//    Medium quality: 50% ~= -75db
-//    Low quality: 30% ~= -85db
-//    Unusable quality: 8% ~= -96db
+    private double meanRss;        // for RP (-50 to -100)
+    // High quality: 90% ~= -55db
+    // Medium quality: 50% ~= -75db
+    // Low quality: 30% ~= -85db
+    // Unusable quality: 8% ~= -96db
 
-    public AccessPoint() {
-    }
+    public AccessPoint() { }
 
     public AccessPoint(AccessPoint another) {
         this.id = UUID.randomUUID().toString();
@@ -49,93 +42,51 @@ public class AccessPoint extends RealmObject implements Parcelable {
         this.meanRss = another.meanRss;
     }
 
-    public String getId() {
-        return id;
-    }
+    public String getId() { return id; }
 
-    public void setId(String id) {
-        this.id = id;
-    }
+    public void setId(String id) { this.id = id; }
 
-    public Date getCreatedAt() {
-        return createdAt;
-    }
+    public Date getCreatedAt() { return createdAt; }
 
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
+    public void setCreatedAt(Date createdAt) { this.createdAt = createdAt; }
 
-    public String getDescription() {
-        return description;
-    }
+    public String getDescription() { return description; }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+    public void setDescription(String description) { this.description = description; }
 
-    public String getSsid() {
-        return ssid;
-    }
+    public String getSsid() { return ssid; }
 
-    public void setSsid(String ssid) {
-        this.ssid = ssid;
-    }
+    public void setSsid(String ssid) { this.ssid = ssid; }
 
-    public String getMac_address() {
-        return mac_address;
-    }
+    public String getMac_address() { return mac_address; }
 
-    public void setMac_address(String mac_address) {
-        this.mac_address = mac_address;
-    }
+    public void setMac_address(String mac_address) { this.mac_address = mac_address; }
 
-    public double getX() {
-        return x;
-    }
+    public double getX() { return x; }
 
-    public void setX(double x) {
-        this.x = x;
-    }
+    public void setX(double x) { this.x = x; }
 
-    public double getY() {
-        return y;
-    }
+    public double getY() { return y; }
 
-    public void setY(double y) {
-        this.y = y;
-    }
+    public void setY(double y) { this.y = y; }
 
-    public int getSequence() {
-        return sequence;
-    }
+    public int getSequence() { return sequence; }
 
-    public void setSequence(int sequence) {
-        this.sequence = sequence;
-    }
+    public void setSequence(int sequence) { this.sequence = sequence; }
 
-    public String getBssid() {
-        return bssid;
-    }
+    public String getBssid() { return bssid; }
 
-    public void setBssid(String bssid) {
-        this.bssid = bssid;
-    }
+    public void setBssid(String bssid) { this.bssid = bssid; }
 
-    public double getMeanRss() {
-        return meanRss;
-    }
+    public double getMeanRss() { return meanRss; }
 
-    public void setMeanRss(double meanRss) {
-        this.meanRss = meanRss;
-    }
+    public void setMeanRss(double meanRss) { this.meanRss = meanRss; }
 
     @Override
-    public int describeContents() {
-        return 0;
-    }
+    public int describeContents() { return 0; }
 
     @Override
-    public void writeToParcel(Parcel parcel, int i) {
+    public void writeToParcel(Parcel parcel, int flags) {
         parcel.writeString(id);
         parcel.writeString(bssid);
         parcel.writeString(description);
@@ -145,6 +96,7 @@ public class AccessPoint extends RealmObject implements Parcelable {
         parcel.writeDouble(y);
         parcel.writeInt(sequence);
         parcel.writeDouble(meanRss);
+        // Note: createdAt not parceled; add if needed
     }
 
     protected AccessPoint(Parcel in) {
@@ -157,17 +109,15 @@ public class AccessPoint extends RealmObject implements Parcelable {
         y = in.readDouble();
         sequence = in.readInt();
         meanRss = in.readDouble();
+        // Note: createdAt not parceled; add if needed
     }
 
     public static final Creator<AccessPoint> CREATOR = new Creator<AccessPoint>() {
         @Override
-        public AccessPoint createFromParcel(Parcel in) {
-            return new AccessPoint(in);
-        }
+        public AccessPoint createFromParcel(Parcel in) { return new AccessPoint(in); }
 
         @Override
-        public AccessPoint[] newArray(int size) {
-            return new AccessPoint[size];
-        }
+        public AccessPoint[] newArray(int size) { return new AccessPoint[size]; }
     };
 }
+```
