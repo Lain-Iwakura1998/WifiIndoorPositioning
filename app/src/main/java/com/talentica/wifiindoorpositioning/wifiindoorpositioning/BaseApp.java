3f0ef9a13b647262ab8643b7fad9b842129aff3c
Lain-Iwakura1998 <ilain3902@gmail.com>
2025-09-04 17:22:39 +0800

    Update BaseApp.java

--- app/src/main/java/com/talentica/wifiindoorpositioning/wifiindoorpositioning/BaseApp.java
+++ app/src/main/java/com/talentica/wifiindoorpositioning/wifiindoorpositioning/BaseApp.java
@@ -2,8 +2,6 @@ package com.talentica.wifiindoorpositioning.wifiindoorpositioning;
 
 import android.app.Application;
 
-import io.realm.Realm;
-
 /**
  * Created by suyashg on 25/08/17.
  */
@@ -13,6 +11,7 @@ public class BaseApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Realm.init(this);
+        // 这里原来是 Realm.init(this)，现在已经不需要了
+        // 如果以后要初始化其他全局组件，可以放在这里
     }
 }
