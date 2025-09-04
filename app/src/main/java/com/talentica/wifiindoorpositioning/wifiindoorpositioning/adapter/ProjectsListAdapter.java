dd129b2bc6ec8e1f616cd86627691b5f0ed49ef5
Lain-Iwakura1998 <ilain3902@gmail.com>
2025-09-04 16:22:52 +0800

    Update ProjectsListAdapter.java

--- app/src/main/java/com/talentica/wifiindoorpositioning/wifiindoorpositioning/adapter/ProjectsListAdapter.java
+++ app/src/main/java/com/talentica/wifiindoorpositioning/wifiindoorpositioning/adapter/ProjectsListAdapter.java
@@ -9,20 +9,17 @@ import android.widget.TextView;
 import com.talentica.wifiindoorpositioning.wifiindoorpositioning.R;
 import com.talentica.wifiindoorpositioning.wifiindoorpositioning.model.IndoorProject;
 
-import io.realm.RealmResults;
+import java.util.List;
 
 /**
  * Created by suyashg on 25/08/17.
  */
 
 public class ProjectsListAdapter extends RecyclerView.Adapter<ProjectsListAdapter.ViewHolder> {
-    private RealmResults<IndoorProject> mDataset;
+    private List<IndoorProject> mDataset;
 
     // Provide a reference to the views for each data item
-// Complex data items may need more than one view per item, and
-// you provide access to all the views for a data item in a view holder
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        // each data item is just a string in this case
         public TextView name;
 
         public ViewHolder(LinearLayout v) {
@@ -31,34 +28,25 @@ public class ProjectsListAdapter extends RecyclerView.Adapter<ProjectsListAdapte
         }
     }
 
-    // Provide a suitable constructor (depends on the kind of dataset)
-    public ProjectsListAdapter(RealmResults<IndoorProject> myDataset) {
+    // Constructor now accepts a List instead of RealmResults
+    public ProjectsListAdapter(List<IndoorProject> myDataset) {
         mDataset = myDataset;
     }
 
-    // Create new views (invoked by the layout manager)
     @Override
     public ProjectsListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        // create a new view
         LinearLayout linearLayout = (LinearLayout) LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.item_project_list, parent, false);
-        // set the view's size, margins, paddings and layout parameters
-        ViewHolder vh = new ViewHolder(linearLayout);
-        return vh;
+        return new ViewHolder(linearLayout);
     }
 
-    // Replace the contents of a view (invoked by the layout manager)
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        // - get element from your dataset at this position
-        // - replace the contents of the view with that element
         holder.name.setText(mDataset.get(position).getName());
-
     }
 
-    // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
-        return mDataset.size();
+        return mDataset != null ? mDataset.size() : 0;
     }
 }
