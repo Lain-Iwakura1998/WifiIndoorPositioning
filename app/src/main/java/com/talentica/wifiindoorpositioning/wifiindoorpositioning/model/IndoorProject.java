f74d76f2b9d95e7ca40ce5913c5b8cda758abd3f
Lain-Iwakura1998 <ilain3902@gmail.com>
2025-09-04 16:00:40 +0800

    Update IndoorProject.java

--- app/src/main/java/com/talentica/wifiindoorpositioning/wifiindoorpositioning/model/IndoorProject.java
+++ app/src/main/java/com/talentica/wifiindoorpositioning/wifiindoorpositioning/model/IndoorProject.java
@@ -1,123 +1,76 @@
 package com.talentica.wifiindoorpositioning.wifiindoorpositioning.model;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
-import java.util.Calendar;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 /**
  * Created by suyashg on 25/08/17.
  */
-public class AccessPoint implements Parcelable {
+public class IndoorProject {
 
     private String id = UUID.randomUUID().toString();
     private Date createdAt = new Date();
-    private String bssid;          // identifier
-    private String description;
-    private String ssid;
-    private String mac_address;
-    private double x;
-    private double y;
-    private int sequence;
-    private double meanRss;        // for RP (-50 to -100)
-    // High quality: 90% ~= -55db
-    // Medium quality: 50% ~= -75db
-    // Low quality: 30% ~= -85db
-    // Unusable quality: 8% ~= -96db
+    private String name;
+    private String desc;
+    private List<AccessPoint> aps = new ArrayList<>();
+    private List<ReferencePoint> rps = new ArrayList<>();
 
-    public AccessPoint() { }
-
-    public AccessPoint(AccessPoint another) {
-        this.id = UUID.randomUUID().toString();
-        this.createdAt = Calendar.getInstance().getTime();
-        this.bssid = another.bssid;
-        this.description = another.description;
-        this.ssid = another.ssid;
-        this.mac_address = another.mac_address;
-        this.x = another.x;
-        this.y = another.y;
-        this.sequence = another.sequence;
-        this.meanRss = another.meanRss;
+    public IndoorProject() {
     }
 
-    public String getId() { return id; }
-
-    public void setId(String id) { this.id = id; }
-
-    public Date getCreatedAt() { return createdAt; }
-
-    public void setCreatedAt(Date createdAt) { this.createdAt = createdAt; }
-
-    public String getDescription() { return description; }
-
-    public void setDescription(String description) { this.description = description; }
-
-    public String getSsid() { return ssid; }
-
-    public void setSsid(String ssid) { this.ssid = ssid; }
-
-    public String getMac_address() { return mac_address; }
-
-    public void setMac_address(String mac_address) { this.mac_address = mac_address; }
-
-    public double getX() { return x; }
-
-    public void setX(double x) { this.x = x; }
-
-    public double getY() { return y; }
-
-    public void setY(double y) { this.y = y; }
-
-    public int getSequence() { return sequence; }
-
-    public void setSequence(int sequence) { this.sequence = sequence; }
-
-    public String getBssid() { return bssid; }
-
-    public void setBssid(String bssid) { this.bssid = bssid; }
-
-    public double getMeanRss() { return meanRss; }
-
-    public void setMeanRss(double meanRss) { this.meanRss = meanRss; }
-
-    @Override
-    public int describeContents() { return 0; }
-
-    @Override
-    public void writeToParcel(Parcel parcel, int flags) {
-        parcel.writeString(id);
-        parcel.writeString(bssid);
-        parcel.writeString(description);
-        parcel.writeString(ssid);
-        parcel.writeString(mac_address);
-        parcel.writeDouble(x);
-        parcel.writeDouble(y);
-        parcel.writeInt(sequence);
-        parcel.writeDouble(meanRss);
-        // Note: createdAt not parceled; add if needed
+    public IndoorProject(Date createdAt, String name, String desc) {
+        this.createdAt = createdAt;
+        this.name = name;
+        this.desc = desc;
     }
 
-    protected AccessPoint(Parcel in) {
-        id = in.readString();
-        bssid = in.readString();
-        description = in.readString();
-        ssid = in.readString();
-        mac_address = in.readString();
-        x = in.readDouble();
-        y = in.readDouble();
-        sequence = in.readInt();
-        meanRss = in.readDouble();
-        // Note: createdAt not parceled; add if needed
+    public String getDesc() {
+        return desc;
     }
 
-    public static final Creator<AccessPoint> CREATOR = new Creator<AccessPoint>() {
-        @Override
-        public AccessPoint createFromParcel(Parcel in) { return new AccessPoint(in); }
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
 
-        @Override
-        public AccessPoint[] newArray(int size) { return new AccessPoint[size]; }
-    };
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<AccessPoint> getAps() {
+        return aps;
+    }
+
+    public void setAps(List<AccessPoint> aps) {
+        this.aps = aps;
+    }
+
+    public List<ReferencePoint> getRps() {
+        return rps;
+    }
+
+    public void setRps(List<ReferencePoint> rps) {
+        this.rps = rps;
+    }
 }
-```
